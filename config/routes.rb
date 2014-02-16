018caf5147f751ce51ae73b72f5987ac978dfc95
sivas2811 <sivas@cs.wisc.edu>
2014-02-16 17:47:05 -0600

    plaint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ FirstApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   resources :users
-  root :to => 'users#index'
+  root :to => 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
